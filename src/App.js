ae2a4659a8a37528992596d7d1f28f406b55e750
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-06 21:37:49 +0600

    practice firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import app from './firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from 'react';
+
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.log('error', error);
+
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {
+        user.email ? <button onClick={handleGoogleSignOut}>sign out</button>
+
+          : <button onClick={handleGoogleSignIn}>sign in</button>
+
+      }
+
+      <h1>name: {user.displayName}</h1>
+      <p>email: {user.email}</p>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBl4f9pEXPZ0zdWBlRh4zuhuRP5eL5Zb9U",
+    authDomain: "fir-practice-b7226.firebaseapp.com",
+    projectId: "fir-practice-b7226",
+    storageBucket: "fir-practice-b7226.appspot.com",
+    messagingSenderId: "573041682940",
+    appId: "1:573041682940:web:6ffa5a62e2def93fa12a79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
